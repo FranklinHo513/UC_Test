b1f4c0ea4a39ce7a91a2ff47dac403a87b4eed33
FranklinHo513 <franklinsmh213@gmail.com>
2020-10-28 16:48:34 +0800

    updated

--- American.py
+++ American.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 r = requests.get("https://api.covidtracking.com/v1/us/daily.json")
 a = json.loads(r.content)
 df = pd.DataFrame.from_dict(a)
+print(df.columns)
 useful_df = df[['date', 'positiveIncrease']]
 useful_df['date'] = pd.to_datetime(useful_df['date'], format='%Y%m%d')
 useful_df.set_index('date', inplace=True)
