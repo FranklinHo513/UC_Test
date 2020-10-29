e1bc6f084c72c7fb882088435f0423cab9c3072e
FranklinHo513 <franklinsmh213@gmail.com>
2020-10-29 17:35:10 +0800

    updated

--- American.py
+++ American.py
@@ -9,6 +9,10 @@ df = pd.DataFrame.from_dict(a)
 print(df.columns)
 useful_df = df[['date', 'positiveIncrease']]
 useful_df['date'] = pd.to_datetime(useful_df['date'], format='%Y%m%d')
+useful_df.rename(columns={'positiveIncrease': 'Daily Case', 'date': 'Report date'}, inplace=True)
+useful_df = useful_df[useful_df['Report date'] >= '2020/03/01']
+useful_df = useful_df[useful_df['Report date'] <= '2020/05/30']
 print(useful_df)
-useful_df.plot(x='date')
+useful_df.plot(x='Report date', title='US')
+plt.axvline(x='2020/03/16', c='r')
 plt.show()
--- TW.py
+++ TW.py
@@ -11,6 +11,7 @@ a = json.loads(r.content)
 df = pd.DataFrame.from_dict(a)
 df.rename(columns={'個案研判日': 'Report date', '是否為境外移入': 'Import'}, inplace=True)
 useful_df = df[['Report date', 'Import']]
+useful_df['Report date'] = pd.to_datetime(useful_df['Report date'], format='%Y/%m/%d')
 useful_df.replace(to_replace='是', value='Yes', inplace=True)
 useful_df.replace(to_replace='否', value='No', inplace=True)
 dummy = pd.get_dummies(useful_df['Import'])
@@ -19,7 +20,12 @@ useful_df.drop('Import', axis=1, inplace=True)
 useful_df = useful_df.groupby('Report date').aggregate({'No': 'sum', 'Yes': 'sum'})
 useful_df.rename(columns={'No': 'Local Case', 'Yes': 'Imported Case'}, inplace=True)
 useful_df.reset_index(inplace=True)
-useful_df = useful_df[useful_df['Report date'] >= '2020/09/20']
+useful_df = useful_df.reindex(columns=['Report date', 'Imported Case', 'Local Case'])
+#useful_df = useful_df[useful_df['Report date'] >= '2020/01/22']
+useful_df = useful_df[useful_df['Report date'] <= '2020/03/30']
 print(useful_df)
-useful_df.plot(x='Report date')
+useful_df.plot(x='Report date', title='Taiwan')
+plt.axvline(x='2020/02/06', c='r')
+#plt.axvline(x='2020/03/25', c='orange')
+plt.ylim(-0.5, 25)
 plt.show()
